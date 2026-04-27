4cba74dfd116b2e90ff849eaf8966c6a49050872
Cristhian Melo <rxtsel@outlook.com>
2026-04-27 13:24:30 -0500

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A devtools for coincli project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+        rust-analyzer
+        rustfmt
+        clippy
+      ];
+    };
+  };
+}
